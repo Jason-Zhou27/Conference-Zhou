671f3538fb8eac12f7f19e26181d25700277f6ad
Jason-Zhou27 <146095415+Jason-Zhou27@users.noreply.github.com>
2025-11-21 14:34:10 +0000

    fixed IOException error with try catch strucutre

--- Conference.java
+++ Conference.java
@@ -20,19 +20,23 @@ public class Conference {
         tables = new int[numTables][pplPerTable];
         fillArray();
     }
-    public void readFileA() throws IOException {
-        Scanner scan = new Scanner(new File(filenameA));
-        int i =0;
-        while (i<aArraySize && scan.hasNext()){
-            String line = scan.nextLine();
-            String[] elements = line.split(",");
-            int id = Integer.parseInt(elements[0]);
-            String lN = elements[1];
-            String fN = elements[2];
-            int cN = Integer.parseInt(elements[3]);
-            conferenceArray[i]=new Attendee(fN, lN, id, cN);
-            i++;
-        } 
+    public void readFileA() {
+        try {
+            Scanner scan = new Scanner(new File(filenameA));
+            int i =0;
+            while (i<aArraySize && scan.hasNext()){
+                String line = scan.nextLine();
+                String[] elements = line.split(",");
+                int id = Integer.parseInt(elements[0]);
+                String lN = elements[1];
+                String fN = elements[2];
+                int cN = Integer.parseInt(elements[3]);
+                conferenceArray[i]=new Attendee(fN, lN, id, cN);
+                i++;
+            }
+        } catch (FileNotFoundException e){
+            System.out.println("File not Found!");
+        }
     }
     //methods
     public void fillArray(){
